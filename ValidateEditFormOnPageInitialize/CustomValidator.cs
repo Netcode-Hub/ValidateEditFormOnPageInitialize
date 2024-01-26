b29d5f63d5aa08a51cb3a1e2544835d4bb1abfcc
Frederick Asante <Rastinga1995@hotmail.com>
2024-01-26 12:35:37 +0000

    Add project files.

---
+++ ValidateEditFormOnPageInitialize/CustomValidator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace ValidateEditFormOnPageInitialize
+{
+    public class CustomValidator : ComponentBase
+    {
+        [CascadingParameter]
+        private EditContext myCustomContext { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            myCustomContext.Validate();
+        }
+    }
+}
